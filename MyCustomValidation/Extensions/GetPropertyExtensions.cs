d530928e4dea61aaa92f9e8340daa44e11d40b8e
emir57 <emir.gurbuz06@hotmail.com>
2022-01-20 19:32:25 +0300

    GetProp Function

---
+++ MyCustomValidation/Extensions/GetPropertyExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyCustomValidation.Extensions
+{
+    public static class GetPropertyExtensions
+    {
+        public static TProperty GetProp<TMethod,TProperty>(this TMethod obj,Expression<Func<TMethod,TProperty>> expression)
+        {
+
+        }
+    }
+}
