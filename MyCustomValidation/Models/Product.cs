4a5580e70b0ddae4ed1ecb55ef7609dd680ac65f
emir57 <emir.gurbuz06@hotmail.com>
2022-01-20 19:35:37 +0300

    Add Product Model

---
+++ MyCustomValidation/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCustomValidation.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public byte Stock { get; set; }
+    }
+}
