9a71df7fddd6955d3dda579cbcedc2055aa9b4d4
emir57 <emir.gurbuz06@hotmail.com>
2022-02-01 01:12:27 +0300

    StartsWithExtension

---
+++ MyCustomValidation/Extensions/StartsWithExtension.cs
@@ -0,0 +1,29 @@
+﻿using MyCustomValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCustomValidation.Extensions
+{
+    public static class StartsWithExtension
+    {
+        /// <summary>
+        /// field is starts with
+        /// </summary>
+        /// <typeparam name="TObject"></typeparam>
+        /// <param name="transferObject"></param>
+        /// <param name="startsWith">startwith parameter</param>
+        /// <returns></returns>
+        public static TransferObject<TObject> StartsWith<TObject>(this TransferObject<TObject> transferObject, string startsWith)
+        {
+            object value; string objectName;
+            ValidateExtensions.GetProps(transferObject, out value, out objectName);
+            if (!value.ToString().StartsWith(startsWith))
+            {
+                ValidateExtensions.ValidationResults.Add(new ValidationResult
+                { Message = $"{objectName} is start with {startsWith}" });
+            }
+            return transferObject;
+        }
+    }
+}
--- MyCustomValidation/Extensions/ValidateExtensions.cs
+++ MyCustomValidation/Extensions/ValidateExtensions.cs
@@ -15,28 +15,6 @@ namespace MyCustomValidation.Extensions
         {
             ValidationResults = new List<ValidationResult>();
         }
-        
-        
-        
-        
-        /// <summary>
-        /// field is starts with
-        /// </summary>
-        /// <typeparam name="TObject"></typeparam>
-        /// <param name="transferObject"></param>
-        /// <param name="startsWith">startwith parameter</param>
-        /// <returns></returns>
-        public static TransferObject<TObject> StartsWith<TObject>(this TransferObject<TObject> transferObject,string startsWith)
-        {
-            object value;string objectName;
-            GetProps(transferObject, out value, out objectName);
-            if (!value.ToString().StartsWith(startsWith))
-            {
-                ValidationResults.Add(new ValidationResult 
-                    { Message = $"{objectName} is start with {startsWith}" });
-            }
-            return transferObject;
-        }
         public static void GetProps<TObject>(TransferObject<TObject> transferObject,out object value,out string objectName)
         {
             value = transferObject.GetValue();
