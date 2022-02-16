23215d736c2a40376692575b16ca6bfc1c0bde68
emir57 <emir.gurbuz06@hotmail.com>
2022-02-16 17:54:54 +0300

    max value in TurkishLanguage

--- MyCustomValidation/Language/TurkishLanguage.cs
+++ MyCustomValidation/Language/TurkishLanguage.cs
@@ -8,7 +8,8 @@ namespace MyCustomValidation.Language
     {
         public string MaxValueMessage(string objectName, object maximumValue)
         {
-            throw new NotImplementedException();
+            string message = $"{objectName} alanı maksimum {maximumValue} karakter olabilir";
+            return message;
         }
 
         public string MinValueMessage(string objectName, object minimumValue)
