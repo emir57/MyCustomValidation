ea075e15fb0dc983adb4ea6c42473a4f604f3b10
emir57 <emir.gurbuz06@hotmail.com>
2022-02-16 17:49:58 +0300

    Add EnglishLanguage

---
+++ MyCustomValidation/Language/EnglishLanguage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCustomValidation.Language
+{
+    public class EnglishLanguage : ILanguage
+    {
+        public string MaxValueMessage(string objectName, object maximumValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string MinValueMessage(string objectName, object minimumValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string MoreThanMessage(string objectName, object moreThanValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string MoreThanOrEqualToMessage(string objectName, object moreThanOrEqualTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StartsWithMessage(string objectName, string startsWith)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
