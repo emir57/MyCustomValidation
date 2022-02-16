8281199a96cb737affa619bf740247513ffee95c
emir57 <emir.gurbuz06@hotmail.com>
2022-02-16 18:02:57 +0300

    more than or equal to message

--- MyCustomValidation/Extensions/MoreThanOrEqualToExtension.cs
+++ MyCustomValidation/Extensions/MoreThanOrEqualToExtension.cs
@@ -1,4 +1,5 @@
 ﻿using MyCustomValidation.Models;
+using MyCustomValidation.Validation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,7 +21,7 @@ namespace MyCustomValidation.Extensions
             if (Convert.ToInt64(moreThanOrEqualTo) > Convert.ToInt64(value))
             {
                 ValidateExtensions.ValidationResults.Add(new ValidationResult
-                { Message = $"{objectName} is more than or equal to {moreThanOrEqualTo}" });
+                { Message = ValidateSettings.Language.MaxValueMessage(objectName, moreThanOrEqualTo) });
             }
             return transferObject;
         }
