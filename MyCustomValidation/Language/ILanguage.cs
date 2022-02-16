d8905dcbbeecdcb5e6134cac78b43b65e32b9791
emir57 <emir.gurbuz06@hotmail.com>
2022-02-16 17:49:28 +0300

    ILanguage

---
+++ MyCustomValidation/Language/ILanguage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCustomValidation.Language
+{
+    public interface ILanguage
+    {
+        string MaxValueMessage(string objectName, object maximumValue);
+        string MinValueMessage(string objectName, object minimumValue);
+        string MoreThanMessage(string objectName, object moreThanValue);
+        string MoreThanOrEqualToMessage(string objectName, object moreThanOrEqualTo);
+        string StartsWithMessage(string objectName, string startsWith);
+    }
+}
